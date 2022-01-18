958a554a73df1749210210fa0877044dc2af56a8
This2sho <zx00018@naver.com>
2022-01-19 02:13:29 +0900

    Test: 자바의 기본 값 타입 확인 테스트
    자바의 Wrapper 클래스의 값타입은 값을 공유하지 않는다.

---
+++ src/main/java/jpabook/jpashop/domain/ValueMain.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.domain;
+
+public class ValueMain {
+    public static void main(String[] args) {
+
+        Integer a = 10;
+        Integer b = a;
+
+        a = 20;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
